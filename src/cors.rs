6dadbee96d4ccf30775a29858507ab40a0c3af8d
Torben Carstens <github@carstens.tech>
2020-12-31 18:50:59 +0100

    Add CORS_ALLOW_ORIGIN as env var

--- src/cors.rs
+++ src/cors.rs
@@ -1,8 +1,14 @@
+use std::env;
+
 use rocket::{Request, Response};
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Header;
 
 pub struct CORS();
+
+lazy_static! {
+    static ref CORS_ALLOW_ORIGIN: &str = env::var("CORS_ALLOW_ORIGIN").unwrap_or("*");
+}
 
 impl Fairing for CORS {
     fn info(&self) -> Info {
